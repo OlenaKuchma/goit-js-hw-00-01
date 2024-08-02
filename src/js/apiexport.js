6ba33db02f455a77b86a3081f4694f02ab2f8b30
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2024-08-02 13:11:00 +0300

    main

---
+++ src/js/apiexport.js
@@ -0,0 +1,29 @@
+// // -----------------default export
+// function getFetchAllUsers(params) {
+//   console.log('getFetchAllUsers');
+// }
+
+// function fetchUserById(id) {
+//   console.log('fetchUserById');
+// }
+
+// function updateUserById(id) {
+//   console.log('updateUserById');
+// }
+
+// export default { getFetchAllUsers, fetchUserById, updateUserById };
+
+// // --------------named export
+
+// export function getFetchAllUsers(params) {
+//   console.log('getFetchAllUsers');
+// }
+
+// export function fetchUserById(id) {
+//   console.log('fetchUserById');
+// }
+
+// export function updateUserById(id) {
+//   console.log('updateUserById');
+// }
+// export const number = 5;
