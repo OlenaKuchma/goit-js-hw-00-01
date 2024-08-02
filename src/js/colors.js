dd4a03f030eebd71e4e3b3c5abfa9464abbaad90
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2024-08-02 12:36:22 +0300

    main

---
+++ src/js/colors.js
@@ -0,0 +1,23 @@
+export const colors = [
+  { hex: '#FF5733', rgb: '255,87,51' }, // Red-Orange
+  { hex: '#33FF57', rgb: '51,255,87' }, // Lime Green
+  { hex: '#3357FF', rgb: '51,87,255' }, // Blue
+  { hex: '#F4A300', rgb: '244,163,0' }, // Orange
+  { hex: '#D9534F', rgb: '217,83,79' }, // Red
+  { hex: '#5BC0DE', rgb: '91,192,222' }, // Light Blue
+  { hex: '#F0AD4E', rgb: '240,173,78' }, // Yellow-Orange
+  { hex: '#5E5E5E', rgb: '94,94,94' }, // Gray
+  { hex: '#E7B55F', rgb: '231,181,95' }, // Light Gold
+  { hex: '#4C9F70', rgb: '76,159,112' }, // Green
+  { hex: '#9B59B6', rgb: '155,89,182' }, // Purple
+  { hex: '#E74C3C', rgb: '231,76,60' }, // Bright Red
+  { hex: '#2ECC71', rgb: '46,204,113' }, // Emerald Green
+  { hex: '#3498DB', rgb: '52,152,219' }, // Bright Blue
+  { hex: '#F39C12', rgb: '243,156,18' }, // Bright Orange
+  { hex: '#1ABC9C', rgb: '26,188,156' }, // Turquoise
+  { hex: '#D35400', rgb: '211,84,0' }, // Orange-Red
+  { hex: '#7F8C8D', rgb: '127,140,141' }, // Grayish
+  { hex: '#2980B9', rgb: '41,128,185' }, // Dark Blue
+  { hex: '#C0392B', rgb: '192,57,43' }, // Dark Red
+  { hex: '#BDC3C7', rgb: '189,195,199' }, // Light Gray
+];
