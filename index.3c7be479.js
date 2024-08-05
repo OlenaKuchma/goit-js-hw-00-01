16d0e9de15823e122a58d9e9f728b86e6ded5b81
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2024-08-05 14:28:10 +0000

    Deploying to gh-pages from @ OlenaKuchma/goit-js-hw-00-01@2b04b6c172d9f92dda9ad1fe7c755cd32f44bbab 🚀

--- index.3c7be479.js
+++
@@ -1,2 +0,0 @@
-({notification:document.querySelector(".js-alert")}).notification.addEventListener("click",hideNotification);
-//# sourceMappingURL=index.3c7be479.js.map
--- index.5222f536.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".js-alert").addEventListener("click",hideNotification);
-//# sourceMappingURL=index.5222f536.js.map
