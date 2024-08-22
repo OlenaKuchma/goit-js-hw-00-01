aeb07280ff928175d2f6a04d860890ea717ec94c
OlenaKuchma <125207379+OlenaKuchma@users.noreply.github.com>
2024-08-22 06:57:05 +0000

    Deploying to gh-pages from @ OlenaKuchma/goit-js-hw-00-01@682cb44d6a553e09b041eaed72b82a718fbf45a8 🚀

---
+++ index.128a7267.js
@@ -0,0 +1,2 @@
+refs.startBtn.addEventListener("click",(()=>{timer.start()})),refs.stopBtn.addEventListener("click",(()=>{timer.stop()}));
+//# sourceMappingURL=index.128a7267.js.map
---
+++ index.69db1f48.js
@@ -0,0 +1,2 @@
+refs.startBtn.addEventListener("click",(function(){timer.start()})),refs.stopBtn.addEventListener("click",(function(){timer.stop()}));
+//# sourceMappingURL=index.69db1f48.js.map
